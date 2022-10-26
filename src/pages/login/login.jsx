4ecc5028f0ab61c50a75c8e2a8f44fdfc8a19593
fardenteam <fardenincteam@gmail.com>
2022-10-27 00:42:00 +0700

    Project 1

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,20 @@
+import "./login.scss"
+
+const login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+            <div className="Left">
+                  <h1>FARDEN LOGIN</h1>
+                  <p>
+                        Selamat datang pengguna baru
+                  </p>
+                  <span>Tidak mempunyai akun?</span>
+            </div>
+            <div className="Right"></div>
+      </div>
+    </div>
+  )
+}
+
+export default login
